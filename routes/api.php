010b7ed6be8c7a21a0e64ded586d5542076423cc
Oussama Benkejjane <oussama.benkejjane@gmail.com>
2020-02-27 16:14:59 +0100

    adding laravel passport

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,19 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+Route::group(['middleware' => ['guest:api']], function () {
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('signup', 'Api\AuthController@signup');
+    Route::get('verify-email/{id}/{hash}', 'Api\VerificationController@verify')->name('verification.verify');
+});
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('resend-email-verification', 'Api\VerificationController@resend');
+    Route::get('logout', 'Api\AuthController@logout');
+    Route::get('getuser', 'Api\AuthController@getUser');
+    Route::get('getusers', 'Api\AuthController@getUsers');
 });
